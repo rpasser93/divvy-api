934eaf36ca85051dbd1d2e45c8145666fe2983f9
Ryan Passer <ryan.passer@praxent.com>
2023-10-18 14:33:42 -0400

    Add functionality to endpoints for manipulating owed and indebted parties within expenses

--- api/routes.py
+++ api/routes.py
@@ -4,7 +4,7 @@ from json import loads
 
 import sys
 sys.path.append('.')
-from database.collections.users import create_new_user, get_all_users, get_user_by_id, get_user_by_login, delete_user, update_user, add_expense_for_user, update_expense_access_for_user, delete_expense_for_user
+from database.collections.users import create_new_user, get_all_users, get_user_by_id, delete_user, update_user, add_expense_for_user, update_expense_access_for_user, delete_expense_for_user
 from database.collections.expenses import create_new_expense, get_all_expenses, get_expense_by_id, update_expense, delete_expense, complete_expense, reopen_expense, add_owed_party, update_owed_party, delete_owed_party, add_indebted_party, update_indebted_party, delete_indebted_party, add_expense_history
 
 app = Flask (__name__)
@@ -16,7 +16,7 @@ def parse_json(data):
 def home():
   return 'Connected to Divvy API.'
 
-##Endpoints for Users
+##Users
 @app.route('/users', methods=['GET', 'POST'])
 def users():
   _json = request.json
@@ -65,7 +65,7 @@ def user_expense(user_id, expense_id):
   if request.method == 'DELETE':
     return delete_expense_for_user(user_id, expense_id)
 
-##Endpoints for Expenses
+##Expenses
 @app.route('/expenses', methods=['GET', 'POST'])
 def expenses():
  _json = request.json
@@ -102,27 +102,57 @@ def single_expense(expense_id):
 
 @app.route('/expenses/<expense_id>/owed', methods=['POST', 'PUT', 'DELETE'])
 def expense_owed_party(expense_id):
+ _json = request.json
  
  if request.method == 'POST':
-  return add_owed_party(expense_id)
+  _first_name = _json['first_name']
+  _last_name = _json['last_name']
+  _amount_owed = _json['amount_owed']
+  return add_owed_party(expense_id, _first_name, _last_name, _amount_owed)
  
  if request.method == 'PUT':
-  return update_owed_party(expense_id)
+  _member_uuid = _json['member_uuid']
+  values_to_update = {}
+  if 'first_name' in _json:
+    values_to_update['first_name'] = _json['first_name']
+  if 'last_name' in _json:
+    values_to_update['last_name'] = _json['last_name']
+  if 'amount_owed' in _json:
+    values_to_update['amount_owed'] = _json['amount_owed']
+  if 'amount_received' in _json:
+    values_to_update['amount_received'] = _json['amount_received']
+  return update_owed_party(expense_id, _member_uuid, values_to_update)
  
  if request.method == 'DELETE':
-  return delete_owed_party(expense_id)
+  _member_uuid = _json['member_uuid']
+  return delete_owed_party(expense_id, _member_uuid)
 
 @app.route('/expenses/<expense_id>/indebted', methods=['POST', 'PUT', 'DELETE'])
 def expense_indebted_party(expense_id):
+  _json = request.json
   
   if request.method == 'POST':
-   return add_indebted_party(expense_id)
+    _first_name = _json['first_name']
+    _last_name = _json['last_name']
+    _amount_owes = _json['amount_owes']
+    return add_indebted_party(expense_id, _first_name, _last_name, _amount_owes)
  
   if request.method == 'PUT':
-   return update_indebted_party(expense_id)
+   _member_uuid = _json['member_uuid']
+   values_to_update = {}
+   if 'first_name' in _json:
+    values_to_update['first_name'] = _json['first_name']
+   if 'last_name' in _json:
+    values_to_update['last_name'] = _json['last_name']
+   if 'amount_owes' in _json:
+    values_to_update['amount_owes'] = _json['amount_owes']
+   if 'amount_paid' in _json:
+    values_to_update['amount_paid'] = _json['amount_paid']
+   return update_indebted_party(expense_id, _member_uuid, values_to_update)
   
   if request.method == 'DELETE':
-   return delete_indebted_party(expense_id)
+   _member_uuid = _json['member_uuid']
+   return delete_indebted_party(expense_id, _member_uuid)
   
 @app.route('/expenses/<expense_id>/complete', methods=['POST'])
 def expense_completed(expense_id):
--- database/collections/expenses.py
+++ database/collections/expenses.py
@@ -5,6 +5,7 @@ from database.constants.status_enums import Status
 from database.constants.access_enums import Access
 from database.helpers.parse_json import parse_json
 from datetime import datetime
+from uuid import uuid4
 from flask import abort, Response
 
 def create_new_expense(name: str, description: str, date_of_expense: datetime, creator_id: str):
@@ -97,24 +98,135 @@ def reopen_expense(id: str):
   updated_expense = parse_json(expenses_collection.find(expense_query))
   return updated_expense[0]
 
+def add_owed_party(id: str, first_name: str, last_name: str, amount_owed: float):
+  expense_query = {'_id': ObjectId(id)}
+  expense = expenses_collection.find(expense_query)
+  parsed_expense = parse_json(expense)
+  if len(parsed_expense) == 0:
+   return abort(Response('Expense with that id does not exist.', 404))
+  
+  duplicate_member_in_owed_party = parse_json(expenses_collection.find({'_id': ObjectId(id), 'owed_party.first_name': first_name, 'owed_party.last_name': last_name}))
+  if len(duplicate_member_in_owed_party) != 0:
+    return abort(Response('An "Owed" member of that name already exists in this expense.', 400))
+  
+  duplicate_member_in_indebted_party = parse_json(expenses_collection.find({'_id': ObjectId(id), 'indebted_party.first_name': first_name, 'indebted_party.last_name': last_name}))
+  if len(duplicate_member_in_indebted_party) != 0:
+    return abort(Response('A member of that name is already marked as "Indebted" in this expense.', 400))
+  
+  new_owed_party = {
+    'uuid': str(uuid4()),
+    'first_name': first_name,
+    'last_name': last_name,
+    'amount_owed': float(amount_owed),
+    'amount_received': 0.00
+  }
+  expenses_collection.update_one(expense_query, {'$push': {'owed_party': new_owed_party}})
+  updated_expense = parse_json(expenses_collection.find(expense_query))
+  return updated_expense[0]
+
+def update_owed_party(expense_id: str, uuid: str, values_to_update: dict):
+  expense_query = {'_id': ObjectId(expense_id), 'owed_party.uuid': uuid}
+  expense = expenses_collection.find(expense_query)
+  parsed_expense = parse_json(expense)
+  if len(parsed_expense) == 0:
+   return abort(Response('Owed member with that uuid does not exist.', 404))
+  
+  updated_values = {}
+
+  if 'first_name' in values_to_update:
+    updated_values['owed_party.$.first_name'] = values_to_update['first_name']
+  if 'last_name' in values_to_update:
+    updated_values['owed_party.$.last_name'] = values_to_update['last_name']
+  if 'amount_owed' in values_to_update:
+    updated_values['owed_party.$.amount_owed'] = float(values_to_update['amount_owed'])
+  if 'amount_received' in values_to_update:
+    updated_values['owed_party.$.amount_received'] = float(values_to_update['amount_received'])
+
+  expenses_collection.update_one(expense_query, {'$set': updated_values })
+  updated_expense = parse_json(expenses_collection.find(expense_query))
+  return updated_expense[0]
+
+def delete_owed_party(expense_id: str, uuid: str):
+  expense_query = {'_id': ObjectId(expense_id)}
+  expense = expenses_collection.find(expense_query)
+  parsed_expense = parse_json(expense)
+  if len(parsed_expense) == 0:
+   return abort(Response('Expense with that id does not exist.', 404))
+  
+  expense_member_query = {'_id': ObjectId(expense_id), 'owed_party.uuid': uuid}
+  expense_member = expenses_collection.find(expense_member_query)
+  parsed_expense_member = parse_json(expense_member)
+  if len(parsed_expense_member) == 0:
+   return abort(Response('Owed member with that uuid does not exist within expense.', 404))
+  
+  expenses_collection.update_one({'_id': ObjectId(expense_id), 'owed_party.uuid': uuid}, {'$pull': {'owed_party': {'uuid': uuid}}})
+  updated_expense = parse_json(expenses_collection.find(expense_query))
+  return updated_expense[0]
+
+def add_indebted_party(id: str, first_name: str, last_name: str, amount_owes: float):
+  expense_query = {'_id': ObjectId(id)}
+  expense = expenses_collection.find(expense_query)
+  parsed_expense = parse_json(expense)
+  if len(parsed_expense) == 0:
+   return abort(Response('Expense with that id does not exist.', 404))
+  
+  duplicate_member_in_indebted_party = parse_json(expenses_collection.find({'_id': ObjectId(id), 'indebted_party.first_name': first_name, 'indebted_party.last_name': last_name}))
+  if len(duplicate_member_in_indebted_party) != 0:
+    return abort(Response('An "Indebted" member of that name already exists in this expense.', 400))
+  
+  duplicate_member_in_owed_party = parse_json(expenses_collection.find({'_id': ObjectId(id), 'owed_party.first_name': first_name, 'owed_party.last_name': last_name}))
+  if len(duplicate_member_in_owed_party) != 0:
+    return abort(Response('A member of that name is already marked as "Owed" in this expense.', 400))
+  
+  new_indebted_party = {
+    'uuid': str(uuid4()),
+    'first_name': first_name,
+    'last_name': last_name,
+    'amount_owes': float(amount_owes),
+    'amount_paid': 0.00
+  }
+  expenses_collection.update_one(expense_query, {'$push': {'indebted_party': new_indebted_party}})
+  updated_expense = parse_json(expenses_collection.find(expense_query))
+  return updated_expense[0]
+
+def update_indebted_party(expense_id: str, uuid: str, values_to_update: dict):
+  expense_query = {'_id': ObjectId(expense_id), 'indebted_party.uuid': uuid}
+  expense = expenses_collection.find(expense_query)
+  parsed_expense = parse_json(expense)
+  if len(parsed_expense) == 0:
+   return abort(Response('Indebted member with that uuid does not exist within expense.', 404))
+  
+  updated_values = {}
+
+  if 'first_name' in values_to_update:
+    updated_values['indebted_party.$.first_name'] = values_to_update['first_name']
+  if 'last_name' in values_to_update:
+    updated_values['indebted_party.$.last_name'] = values_to_update['last_name']
+  if 'amount_owes' in values_to_update:
+    updated_values['indebted_party.$.amount_owes'] = float(values_to_update['amount_owes'])
+  if 'amount_paid' in values_to_update:
+    updated_values['indebted_party.$.amount_paid'] = float(values_to_update['amount_paid'])
+
+  expenses_collection.update_one(expense_query, {'$set': updated_values })
+  updated_expense = parse_json(expenses_collection.find(expense_query))
+  return updated_expense[0]
+
+def delete_indebted_party(expense_id: str, uuid: str):
+  expense_query = {'_id': ObjectId(expense_id)}
+  expense = expenses_collection.find(expense_query)
+  parsed_expense = parse_json(expense)
+  if len(parsed_expense) == 0:
+   return abort(Response('Expense with that id does not exist.', 404))
+  
+  expense_member_query = {'_id': ObjectId(expense_id), 'indebted_party.uuid': uuid}
+  expense_member = expenses_collection.find(expense_member_query)
+  parsed_expense_member = parse_json(expense_member)
+  if len(parsed_expense_member) == 0:
+   return abort(Response('Indebted member with that uuid does not exist within expense.', 404))
+  
+  expenses_collection.update_one({'_id': ObjectId(expense_id), 'indebted_party.uuid': uuid}, {'$pull': {'indebted_party': {'uuid': uuid}}})
+  updated_expense = parse_json(expenses_collection.find(expense_query))
+  return updated_expense[0]
+
 def add_expense_history(id: str, text: str):
-  return f'Updating history for expense: {id} with text: "{text}"'
-
-##owed and indebted
-def add_owed_party(expense_id: str):
-  return f'Adding owed party to expense: {expense_id}'
-
-def update_owed_party(expense_id: str):
-  return f'Updating owed party in expense: {expense_id}'
-
-def delete_owed_party(expense_id: str):
-  return f'Deleting owed party in expense: {expense_id}'
-
-def add_indebted_party(expense_id: str):
-  return f'Adding indebted party to expense in expense: {expense_id}'
-
-def update_indebted_party(expense_id: str):
-  return f'Updating indebted party in expense: {expense_id}'
-
-def delete_indebted_party(expense_id: str):
-  return f'Deleting indebted party in expense: {expense_id}'
+  return f'Updating history for expense: {id} with text: "{text}"\n-{datetime.now()}'
